648afe71ff3928988c4ba04ec45b1748bd2982c2
Hayder Mir <hmir@ciena.com>
2020-12-20 12:52:01 -0500

    Page Updates

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = "Hayder's GitHub Profile Page";
+
+var today = new Date();
+document.getElementById('dtText').innerHTML=today;
+
