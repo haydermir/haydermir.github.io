81081185925f044cfad17691bb4e57582e038dcb
Hayder Mir <hayder.mir@gmail.com>
2020-12-27 17:08:07 -0500

    Delete main.js

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = "Hayder's GitHub Profile Page";
-
-var today = new Date();
-document.getElementById('dtText').innerHTML=today;
-
